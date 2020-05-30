a44807bbdbb24b851cbb3af7bf9dcf8c8e9908ec
Mike Du <mdd@live.com>
2020-05-30 13:33:26 -0400

    got timer working

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,17 +7,48 @@ import {
 } from "../styled/Game";
 import { Strong } from "../styled/Random";
 
-export default function Game() {
+export default function Game({history}) {
   const [score, setScore] = useState(0);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(999);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setScore((prevScore) => prevScore + 1);
-    }, 1000);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
     return () => {
       clearInterval(interval);
     };
-  }, [score]);
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    //add zeros if necessary to ensure the string has exactly 5 characters
+    const formattedMSString = ("0000" + msPassedStr).slice(-5);
+    //0000 - first 2 are the seconds, and the last 3 are the ms
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 -
+      parseInt(formattedMSString.substring(formattedMSString.length - 3));
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+        history.push('/gameOver');
+    }
+}, [seconds, ms, history]);
+
+  const addLeadingZeros = (str, length) => {
+    let zeros = "";
+    for (let i = 0; i < length; i++) {
+      zeros += "0";
+    }
+    return (zeros + str).slice(-length);
+  };
 
   return (
     <StyledGame>
@@ -26,7 +57,10 @@ export default function Game() {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time:<Strong>00: 000</Strong>
+        Time:{" "}
+        <Strong>
+          {seconds}:{ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
