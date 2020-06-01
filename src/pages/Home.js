46596fa987e843a0a391c160b46b194f54b17646
Mike Du <mdd@live.com>
2020-05-31 22:23:18 -0400

    Updated Text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import {Accent, StyledTitle} from '../styled/Random';
 export default function Home() {
   return (
     <div>
-      <StyledTitle>Ready to type?</StyledTitle>
+      <StyledTitle>Ready to TYPE?</StyledTitle>
       <CTA to="/game">Click or type <Accent>'s'</Accent> to start playing!</CTA>
     </div>
   );
